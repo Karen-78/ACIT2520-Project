868a4ed8a7dae3de2e2fd0342d32de7f237cffcb
lara <lara@gmail.com>
2024-04-03 21:00:26 -0700

    delete merge confilct

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-<<<<<<< HEAD
 
 
-=======
->>>>>>> cb9a79db9f42ccf82e2e6a099a655434bd14f83e
+
+
 const express = require("express");
 const app = express();//this is our web server
 const path = require("path");
@@ -17,7 +16,7 @@ const { ensureAuthenticated } = require("./middleware/checkAuth");
 const session = require("express-session");
 
 const FileStore = require('session-file-store')(session);
-const fileStoreOptions = {}; 
+const fileStoreOptions = {};
 
 const passport = require("./middleware/passport");
 const session = require("express-session");
@@ -32,12 +31,12 @@ app.use(express.urlencoded({ extended: false }));//By adding this line of code, 
 
 app.use(ejsLayouts);
 
-<<<<<<< HEAD
+
 app.set("view engine", "ejs");//this line make us must have a folder called views, ejs is a template engine
-=======
+
 
 app.set("view engine", "ejs");
->>>>>>> cb9a79db9f42ccf82e2e6a099a655434bd14f83e
+
 
 app.use(
   session({
@@ -73,17 +72,17 @@ app.use("/auth", authRoute);
 // app.use("/reminder", authRoute);
 
 // Routes start here
-<<<<<<< HEAD
+
 app.get("/reminders", reminderController.list);//case2:users goes to localhost:3001/reminders,show a list of reminders,it will call the list function in reminder_controller.js
 app.get("/reminder/new", reminderController.new);//case3:users goes to localhost:3001/reminder/new, show a create reminder page
 app.get("/reminder/:id", reminderController.listOne);//case5:user wants to SEE an individual reminder.  :id means id is a dynamic value
 app.post("/reminder/", reminderController.create);//case4:user sends(POST) new reminder data to us(creating a new reminder),here is /reminder, because the form action is"/reminder"
-=======
+
 // reminderController is from the file ./controller/reminder_controller
 app.get("/reminders", reminderController.checkAdmin, reminderController.list); // when user goes to localhost:3001/reminders
 app.get("/reminder/new", reminderController.new);
 app.get("/reminder/:id", reminderController.listOne);
->>>>>>> cb9a79db9f42ccf82e2e6a099a655434bd14f83e
+
 app.get("/reminder/:id/edit", reminderController.edit);
 // ⭐ Implement these two routes below!
 app.post("/reminder/update/:id", reminderController.update);//case 7:user click the edit button, and expected to update the reminder
@@ -95,7 +94,7 @@ app.get("/auth/login", authController.login);
 app.post("/register", authController.registerSubmit);
 app.post("/auth/login", authController.loginSubmit);
 
-app.get("/admin", ensureAuthenticated,  (req, res) => {
+app.get("/admin", ensureAuthenticated, (req, res) => {
   const sessionId = req.sessionID;
   const store = req.sessionStore;
   res.render("admin", {
@@ -114,7 +113,7 @@ app.listen(3001, function () {
   );
 });
 
-<<<<<<< HEAD
+
 
 // previous code
 app.use(
@@ -144,5 +143,4 @@ app.use((req, res, next) => {
   next();
 });
 
-=======
->>>>>>> cb9a79db9f42ccf82e2e6a099a655434bd14f83e
+
