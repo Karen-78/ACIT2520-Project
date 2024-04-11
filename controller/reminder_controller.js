ed376428604c7b2156793fc63a8930757d2a6133
Karen <ace.eona@gmail.com>
2024-04-11 12:02:45 -0700

    fix conflicts

--- controller/reminder_controller.js
+++ controller/reminder_controller.js
@@ -1,31 +1,20 @@
 let model = require("../models/userModel");
 
 let remindersController = {
-<<<<<<< HEAD
-  list: (req, res) => {
-    // const user = req.user;
-    console.log(req.user)// can see the currently login user
-    //  if (req.user.role=="admin"){res.redirect("/admin")}
-    res.render("reminder/index", { reminders: database.cindy.reminders });//here is only cindy, but after you login,it should be the user's name
-  },//render really just means take this file and draw it onto the browser screen// it will find the ejs file in views folder
-=======
+
   list: (req, res) => { // returns a list of the user's reminders
     res.render("reminder/index", { reminders: req.user.reminders });
   },
->>>>>>> cb9a79db9f42ccf82e2e6a099a655434bd14f83e
 
   new: (req, res) => { // to create new reminder
     res.render("reminder/create");
   },
 
   listOne: (req, res) => {
-<<<<<<< HEAD
-    let reminderToFind = req.params.id;//here will find the dynamic id in URL,like /reminder/1,1 is the id
-    let searchResult = database.cindy.reminders.find(function (reminder) {//Find will return the value(item or dictionary) of the first element in the list that satisfies some function that we give it
-=======
+
+
     let reminderToFind = req.params.id;
     let searchResult = req.user.reminders.find(function (reminder) {
->>>>>>> cb9a79db9f42ccf82e2e6a099a655434bd14f83e
       return reminder.id == reminderToFind;
     });
     if (searchResult != undefined) {
@@ -39,15 +28,6 @@ let remindersController = {
     // let data= req.body;
     // console.log(data)//you will get: { title: 'hi', description: 'hi again' },title and description are the "name" of the input
     let reminder = {
-<<<<<<< HEAD
-      id: database.cindy.reminders.length + 1,
-      title: req.body.title,//hi
-      description: req.body.description,//hi again
-      completed: false,
-    };
-    database.cindy.reminders.push(reminder);//database.cindy.reminders is a list
-    res.redirect("/reminders");//this page is a list of the whole reminders
-=======
       id: req.user.reminders.length + 1,
       title: req.body.title,
       description: req.body.description,
@@ -56,18 +36,11 @@ let remindersController = {
     };
     req.user.reminders.push(reminder);
     res.redirect("/reminders");
->>>>>>> cb9a79db9f42ccf82e2e6a099a655434bd14f83e
   },
 
   edit: (req, res) => {
     let reminderToFind = req.params.id;
-<<<<<<< HEAD
-    let searchResult = database.cindy.reminders.find(function (reminder) {
-      //find will return the first element in the provided array that satisfies the provided testing function
-=======
-    // change "database.cindy" to req.user (don't hard-code it)
     let searchResult = req.user.reminders.find(function (reminder) {
->>>>>>> cb9a79db9f42ccf82e2e6a099a655434bd14f83e
       return reminder.id == reminderToFind;
       //if reminderToFind= i, will get {id: 1,title: "Grocery shopping", description: "Buy milk and bread from safeway",completed: false}
     });
@@ -103,19 +76,6 @@ let remindersController = {
   },
 
   delete: (req, res) => {
-<<<<<<< HEAD
-    let reminderToDeleteId = req.params.id;
-
-    let reminderIndex = database.cindy.reminders.findIndex(reminder => reminder.id == reminderToDeleteId);
-    if (reminderIndex !== -1) {
-      database.cindy.reminders.splice(reminderIndex, 1);
-      //delete the value which the index = reminderIndex,quantity = 1
-      res.redirect("/reminders");
-    }
-
-
-    // implementation here 👈
-=======
       // implementation here 👈
       let reminderToFind = req.params.id;
       // change "database.cindy" to req.user (don't hard-code it)
@@ -134,7 +94,6 @@ let remindersController = {
     } else {
       res.redirect("/reminders")
     }
->>>>>>> cb9a79db9f42ccf82e2e6a099a655434bd14f83e
   },
 
   admin: (req, res) => {
